c45e6e7e8e65f920af180db8a409c1cb0a412cd0
rahwalegesse <rahwalegesse31@gmail.com>
2023-10-19 08:24:51 -0700

    task4

--- add.c
+++ add.c
@@ -1,32 +1,31 @@
 #include "monty.h"
 /**
- * _insert - it inserts two elements on the top  of the stack.
- * @h:head
- * @c: number of line argument
-*/
+ * _insert - it adds the elements
+ * @h: head
+ * @c: counter
+ */
 void _insert(stack_t **h, unsigned int c)
 {
-	stack_t *head;
-	int length = 0;
-	int tmp;
+	stack_t *hd;
+	int length = 0, tmp;
 
-	head = *h;
-	while (head)
+	hd = *h;
+	while (hd)
 	{
-		head = head->next;
+		hd = hd->next;
 		length++;
 	}
 	if (length < 2)
 	{
-		fprintf(stderr, "L%d: can't insert, stack too short\n", c);
+		fprintf(stderr, "L%d: can't add, stack too short\n", c);
 		fclose(data.fd);
 		free(data.con);
 		stack_free(*h);
 		exit(EXIT_FAILURE);
 	}
-	head = *h;
-	tmp = head->n + head->next->n;
-	head->next->n = tmp;
-	*h = head->next;
-	free(head);
+	hd = *h;
+	tmp = hd->n + hd->next->n;
+	hd->next->n = tmp;
+	*h = hd->next;
+	free(hd);
 }
